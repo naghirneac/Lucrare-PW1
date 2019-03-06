bccd315955a863bbf67132c8ea424947ca7ecb09
Naghirneac Ana <33444068+naghirneac@users.noreply.github.com>
2019-03-06 21:29:08 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
  */
 
 'use strict'
-//ex.1
+// Lucrare PW1 - Naghirneac Ana (is21z)
+//ex.1-------------------------------------
 class Worker{
     constructor(name, surname, hourRate, hours){
     	this.name = name;
@@ -12,7 +13,7 @@ class Worker{
     	this.hours = hours;
     }
 
-   
+
     getName(name){
     	return this.name;
     }
@@ -25,12 +26,18 @@ class Worker{
     getHours(hours){
     	return this.hours;
     }
-    increaseSalary(a){
-    	return (a*this.hourRate);
-    }
-     getSalary(hours, hourRate){
-    	return (increaseSalary()*this.hours);
-    }
+    getSalary(){
+		return this.hourRate*this.hours; 
+	}
+    increaseSalary(n){
+		this.hourRate = n + this.hourRate;
+	}
+	setTax(n){
+		return ((this.getSalary()*n)/100);
+	}
+	getSalaryNetto(n){
+		return (this.getSalary() - this.setTax(n));
+	}
 }
 
 /*let worker = new Worker('Ion', 'Creangă', 10, 176); 
@@ -40,7 +47,7 @@ console.log(worker.hourRate); //afișează 10
 console.log(worker.hours); //afișează 31 
 console.log(worker.getSalary()); //afișează 1760 = 10*176*/
 
-//ex.2
+//ex.2---------------------------------------------------
 /*let primulWorker = new Worker('Mihai', 'Eminescu', 9, 150); 
 /*console.log(primulWorker.name);  
 console.log(primulWorker.surname);  
@@ -55,10 +62,9 @@ console.log(alDoileaWorker.hourRate);
 console.log(alDoileaWorker.hours); 
 console.log(alDoileaWorker.getSalary());*/
 
-//ex.3
+//ex.3---------------------------------------------
 
 /*let max = new Worker;
-
 /*function salariuMaiMare(){
 	if (primulWorker.getSalary() > worker.getSalary()) {
 		max = primulWorker;
@@ -74,7 +80,6 @@ console.log(alDoileaWorker.getSalary());*/
 	console.log(max.hourRate);  
 	console.log(max.hours); 
 	console.log(max.getSalary());
-
 	if (max != worker){
 		console.log(worker.name); 
 		console.log(worker.surname); 
@@ -99,16 +104,47 @@ console.log(alDoileaWorker.getSalary());*/
 		console.log(alDoileaWorker.getSalary());
 		console.log('Salariul lui ', max.name, ' e mai mare cu ', max.getSalary() - alDoileaWorker.getSalary());
 	}
-
 }
 salariuMaiMare();*/
 
-//ex.4 
-var worker = new Worker('Ion', 'Creangă', 10, 176); 
+//ex.4 -----------------------------------------------
+/*var worker = new Worker('Ion', 'Creangă', 10, 176); 
 console.log(worker.getName()); // afișează Ion 
 console.log(worker.getSurname()); // afișează Creangă 
 console.log(worker.getRate()); // afișează 10 
-console.log(worker.getHours()); // afișează 176
+console.log(worker.getHours()); // afișează 176 
+console.log(worker.getSalary()); //afișează 1760 = 10*176*/
+
+//ex.5-------------------------------------------------
+/*var worker = new Worker('Ion', 'Creangă', 10, 176); 
+console.log(worker.getName()); // afișează Ion 
+console.log(worker.getSurname()); // afișează Creangă 
+console.log(worker.getRate()); // afișează 10 
+console.log(worker.getHours()); // afișează 176 
 console.log(worker.getSalary()); //afișează 1760 = 10*176
 worker.increaseSalary(2); //mărește hourRate cu 2
 console.log(worker.getSalary()); //afișează 2112 = 12*176
+ //ex.6---------------------------------------------
+ worker.setTax(18); 
+ //console.log(worker.setTax(18));
+ //console.log(worker.getSalary() - worker.setTax());
+ console.log(worker.getSalaryNetto(18)); //afișează 2112 – 18% = 1731.84*/
+ //ex.7-------------------------------------------
+ function masivAngajati(){
+ 	let ang1 = new Worker('Ion', 'Creanga', 10, 176);
+ 	let ang2 = new Worker('Mihai', 'Eminescu', 9, 150); 
+ 	let ang3 = new Worker('Lucian', 'Blaga', 11, 200); 
+	let angajati = [ang1, ang2, ang3];
+	for(let i = 0; i < angajati.length; i++){
+		console.log('Prenume:', angajati[i].getName());
+		console.log('Nume:', angajati[i].getSurname());
+		console.log('Bani/ora:', angajati[i].getRate());
+		console.log('Ore lucrate:', angajati[i].getHours());
+		angajati[i].increaseSalary(2);
+		console.log('Salariu brut:', angajati[i].getSalary());
+		angajati[i].setTax(18);
+		console.log('Salariu net:', angajati[i].getSalaryNetto(18));
+		console.log('----------------------');
+ 	}
+ }
+masivAngajati();
